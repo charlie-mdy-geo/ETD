bab185ac80d9574711a58c35c6be6087f61cb2b9
ChoChoThet <moetharkaungkincho@gmail.com>
2018-08-19 17:34:38 +0630

    Thesis data insert

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	var database= firebase.database().ref().child("Interns");
 	database.on("child_added", snap => {
-		var project= snap.child("Absrtact ThesiProject").val();
+		var project= snap.child("Abstract").val();
 		var degree = snap.child("Degree").val();
-		var name = snap.child("Name").val();
-		var supervisor= snap.child("Supervisor Name").val();
+		var name = snap.child("Student_name").val();
+		var supervisor= snap.child("Supervisor").val();
 		var title = snap.child("Thesis  Title").val();
-		var year = snap.child("Year").val()
-	$("#interns").append("<tr><td><p>"+ project + "</p></td><td>" + degree + "</td><td>" +
-		name + "</td><td>" + supervisor + "</td><td><p>" + title + "</p></td><td>" + year + "</td></tr>");
+		var year = snap.child("Year").val();
+	$("#interns").append("<tr><td >"+ name + "</td><td>" + degree + "</td><td>" + supervisor
+		 + "</td><td>" + year + "</td><td>" + title + "</td><td>" + project + "</td></tr>");
 });
 });

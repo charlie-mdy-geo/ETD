51ce4a769e35ecf3b8fbcaf43a600c1009f8d562
ChoChoThet <moetharkaungkincho@gmail.com>
2018-08-20 23:07:14 +0630

    Thesis data insert in year

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,30 @@ $(document).ready(function(){
 		var supervisor= snap.child("Supervisor").val();
 		var title = snap.child("Thesis Title").val();
 		var year = snap.child("Year").val();
-	$("#interns").append("<tr><td >"+ name + "</td><td>" + degree + "</td><td>" + supervisor
-		 + "</td><td>" + year + "</td><td>" + title + "</td><td>" + project + "</td></tr>");
-	$("#authors").append("<input type=radio name=radio onclick=author();/>"+ name +"<br><br>");
+		$("#interns").append("<tr><td >"+ name + "</td><td>" + degree + "</td><td>" + supervisor
+			 + "</td><td>" + year + "</td><td>" + title + "</td><td>" + project + "</td></tr>");
+		$("#authors").append("<div class=mdl-list__item><span class=mdl-list__item-primary-content><i class=material-icons mdl-list__item-avatar>person</i>"+ name +"</span></div>");
+	});
+
+	$('#internGetInfo').bind('click', function () {
+		var name = $('input[name="internNameRead"]').val();
+		console.log(name);
+		database.orderByChild('Student_name').equalTo(name).on("child_added", function(snapshot){
+			console.log(snapshot.val());
+			var project= snapshot.child("Abstract").val();
+			var degree = snapshot.child("Degree").val();
+			var name = snapshot.child("Student_name").val();
+			var supervisor= snapshot.child("Supervisor").val();
+			var title = snapshot.child("Thesis Title").val();
+			var year = snapshot.child("Year").val();
+			//$("#internNameResult").val(name);
+			//$("#internIDResult").val(title);
+			//$("#internTeamResult").val(year);
+			$("#internsNameResult").append("<tr><td >"+ name + "</td><td>" + degree + "</td><td>" + supervisor
+			 	+ "</td><td>" + year + "</td><td>" + title + "</td><td>" + project + "</td></tr>");
+		});
+	});
+	//event.preventDefault();
 });
-});
+
+
